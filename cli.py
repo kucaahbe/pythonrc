0c8c71ab83dadf5508bce1f6c0d9fa9523daa89e
kucaahbe <93769+kucaahbe@users.noreply.github.com>
2022-03-09 17:35:10 +0200

    feat: some helpers

--- cli.py
+++ cli.py
@@ -2,5 +2,23 @@ import os
 import sys
 import functools
 import operator
+import json
 import logging
 import itertools
+
+try:
+    import jmespath
+except ModuleNotFoundError as error:
+    logging.warning(error)
+
+class fs:
+    @staticmethod
+    def read(path):
+        content = None
+        with open(path, 'r', encoding='utf-8') as f:
+            content = f.read()
+        return content
+
+    @staticmethod
+    def read_json(path):
+        return json.loads(__class__.read(path))
